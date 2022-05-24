9e6bc4ea2d62cac7703f4c25b8c91f550bb8607f
PavleTomanovic <80409631+PavleTomanovic@users.noreply.github.com>
2022-05-24 21:06:15 +0200

    Finally! :D

--- TourPlanner/BussinesLayer/BussinessLogic.cs
+++ TourPlanner/BussinesLayer/BussinessLogic.cs
@@ -389,18 +389,17 @@ namespace TourPlanner.BussinesLayer
         public List<TourLogDTO> SelectLogForRoute(string routeId)
         {
             DataTable dataTable = DatabaseConnection.Instance.ExecuteSelect(BussinessFactory.Instance.SqlDTO.SelectLogReport, routeId);
-            TourLogDTO tourLogDTO = new TourLogDTO();
             List<TourLogDTO> list = new List<TourLogDTO>();
 
             foreach (DataRow row in dataTable.Rows)
             {
-                tourLogDTO.DateTime = row["DateTime"].ToString();
-                tourLogDTO.Difficulty = row["Difficulty"].ToString();
-                tourLogDTO.Comment = row["Comment"].ToString();
-                tourLogDTO.Rating = row["Rating"].ToString();
-                tourLogDTO.TotalTime = row["TotalTime"].ToString();
+                string dateTime = row["DateTime"].ToString();
+                string difficulty = row["Difficulty"].ToString();
+                string comment = row["Comment"].ToString();
+                string rating = row["Rating"].ToString();
+                string totalTime = row["TotalTime"].ToString();
 
-                list.Add(tourLogDTO);
+                list.Add(new TourLogDTO { DateTime = dateTime, Difficulty = difficulty, Comment = comment, Rating = rating, TotalTime = totalTime });
             }
 
             return list;
